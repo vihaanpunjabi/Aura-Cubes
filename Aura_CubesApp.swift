1d758b72906a732c8a6b8bfba60f26c66252a977
Vihaan Punjabi <vihaan.punjabi11@gmail.com>
2025-04-27 00:02:40 -0700

    Add files via upload

---
+++ Aura_CubesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aura_CubesApp.swift
+//  Aura Cubes
+//
+//  Created by Vihaan on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Aura_CubesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aura Cubes
+//
+//  Created by Vihaan on 8/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
